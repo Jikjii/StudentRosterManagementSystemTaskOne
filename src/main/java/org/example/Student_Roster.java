904d85188a7bfdb90d60ec719f52e7242f0cfc66
jikjii <85137475+Jikjii@users.noreply.github.com>
2026-09-14 15:14:35 -0400

    Student_Roster class updated: Add method;

--- src/main/java/org/example/Student_Roster.java
+++ src/main/java/org/example/Student_Roster.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Student_Roster {
+
+    private ArrayList<Student> classRosterArrayList = new ArrayList<>();
+
+
+    public void add(String studentID, String firstName, String lastName, String email, int age, int grade1, int grade2, int grade3) {
+        int[] grades = {grade1, grade2, grade3};
+        Student student = new Student(studentID, firstName, lastName, email, age, grades);
+        classRosterArrayList.add(student);
+    }
+
+
 }
