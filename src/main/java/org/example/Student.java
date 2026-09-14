37ea93003800b90f2aa18c4e249d191e89de9aa2
jikjii <85137475+Jikjii@users.noreply.github.com>
2026-09-14 14:25:25 -0400

    Getter methods created

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,64 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Student {
+
     public static void main(String[] args) {
-        System.out.println("Hello Student world!");
+
+        Student testStudent = new Student("0000112", "Gerlado", "Grell", "testemail@email.com", 25, new int[]{34,41,41});
+
+        System.out.println("Student age: " + testStudent.getAge() + "   Student name " + Arrays.toString(testStudent.getGrades()));
+}
+
+    // fields
+    private String studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private int[] grades;
+
+    // constructor
+
+    Student(String studentID, String firstName, String lastName, String email, int age, int[] grades) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.grades = grades;
     }
+
+
+    // Getter
+
+    String getStudentID(){
+        return this.studentID;
+    }
+
+    String getFirstName(){
+        return this.firstName;
+    }
+
+    String getLastName(){
+        return this.lastName;
+    }
+
+    String getEmail(){
+        return this.email;
+    }
+
+    int getAge(){
+        return this.age;
+    }
+
+    int[] getGrades(){
+        return this.grades;
+    }
+
+
+
+
+
 }
